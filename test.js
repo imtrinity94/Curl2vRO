1613bf07e7305f1ba6bd02d52088671b421929ac
Mayank Goyal <imtrinity94@gmail.com>
2025-05-04 16:48:57 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const curl2vRO = require('./curl2vRO');
+const curl2vRO = require('curl2vRO');
 
-// Example curl command
-const curlCommand = `curl -O www.haxx.se/index.html -O curl.se/download.html`;
+// Your curl command
+const curlCommand = `curl https://api.example.com/data`;
 
 // Convert to vRO code
 const vroCode = curl2vRO.convertCurlToVRO(curlCommand);
